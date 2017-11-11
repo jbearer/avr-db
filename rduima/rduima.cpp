49c5f02de3e2c09df2c12e4e3196b78287af6ed2
Austin Shin <shinaushin@gmail.com>
2017-11-10 22:45:48 -0800

    debug

--- rduima/rduima.cpp
+++ rduima/rduima.cpp
@@ -62,7 +62,5 @@ char rduima::write(int address, char byte) {
 
 int main() {
     rduima test("/dev/cu.usbmodem1421");
-    test.write(0x2A, 1);
-    cout << "HI" << endl;
-    return 0;
+    return test.write(0x2A, 1);
 }
