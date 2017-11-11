8532f02dd3ad7372cbcd79dd7fe590552ccc96f1
Daniel Zhang <dzhang@hmc.edu>
2017-11-10 20:14:08 -0800

    added private data members

--- simulator/src/segment.cpp
+++ simulator/src/segment.cpp
@@ -1,9 +1,27 @@
 #include "segment.h"
 #include "elfio/elfio.hpp"
+#include <array>
 
 using namespace simulator;
-using namespace ELFIO;
 
-int main() {
-    return 0;
-}
+struct text_segment : segment {
+
+    size_t size() const
+    {
+        return 0;
+    }
+
+
+private:
+
+    static const size_t memory_size = 1024;
+
+    size_t size_;
+    // offset address for the beginning of the segment in virtual memory
+    address_t address_;
+    std::array<byte_t, memory_size> memory;     // holds the physical memory
+
+};
+
+
+
