994ff7699d20a0032c8277d8e8b0972901d3044a
Austin Shin <shinaushin@gmail.com>
2017-11-11 03:11:57 -0800

    holy moly

--- rduima/rduima.cpp
+++ rduima/rduima.cpp
@@ -2,15 +2,23 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
+#include <string.h>     //String function definitions
+#include <unistd.h>     //UNIX standard function definitions
+#include <fcntl.h>      //File control definitions
+#include <errno.h>      //Error number definitions
+#include <termios.h>    //POSIX terminal control definitions
 
 using namespace std;
+
+fstream str;
 
 char rduima::read(int address) {
     ofstream writer;
     writer.open(this->port);
 
     if (writer.is_open()) {
-        writer << 0;
+        writer << 1;
         writer << '\n';
         writer << address;
         writer << '\n';
@@ -23,51 +31,49 @@ char rduima::read(int address) {
 
     //cout << reader.is_open() << endl;
     if (reader.is_open()) {
-        while (buffer == 0) {
-            reader.read(buffer, 1);
-            reader.close();
-            //cout << buffer[0] << endl;
-            return buffer[0];
-        }
+        reader.read(buffer, 1);
+        reader.close();
+        //cout << buffer[0] << endl;
+        return buffer[0];
     }
 
     return 0;
 }
 
-char rduima::write(int address, char byte) {
-    ofstream writer;
-    writer.open(this.port);
+char rduima::write(int address, int byte) {
+    char * ch = new char[100];
     
-    if (writer.is_open()) {
-        writer << 0;
-        writer << '\n';
-        writer << address;
-        writer << '\n';
-        writer << byte;
-        writer << '\n';
-        writer.close();
+    cout << str.is_open() << endl;
+    if (str.is_open()) {
+        usleep(2000000);
+        str << '0' << endl;
+        usleep(2000000);
+        str << address << endl;
+        usleep(2000000);
+        str << byte << endl;
+        usleep(2000000);
     }
 
-    ifstream reader;
-    reader.open(this.port);
-    char * buffer = new char[1];
-
-    cout << reader.is_open() << endl;
-    if (reader.is_open()) {
-        while (buffer == 0) {
-            reader.read(buffer, 1);
-            reader.close();
-            cout << buffer[0] << endl;
-            return buffer[0];
-        }
+    cout << str.is_open() << endl;
+    if (str.is_open()) {
+        //char c = '1';
+        str.get(ch, 1);
+        str.get(ch, 1);
+        str.get(ch, 1);
+        str.get(ch, 1);
+        str.close();
+        cout << ch << endl;
+        return 0;
+        //return buffer[0];
     }
     return 0;
 }
 
 int main() {
     rduima test("/dev/cu.usbmodem1421");
-    test.write(0x60, 'A');
-    test.read(0x60);
+    str.open("/dev/cu.usbmodem1421");
+    test.write(60, 2);
+    //test.read(0x60);
 
     return 0;
 }
--- rduima/rduima.h
+++ rduima/rduima.h
@@ -3,10 +3,10 @@
 struct rduima {
     std::string port;
     rduima(std::string path) : port(path) {
-    	
+
     }
 
     char read(int address);
 
-    char write(int address, char byte);
+    char write(int address, int byte);
 };
